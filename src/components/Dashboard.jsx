39babaef7b9bc7afde2ab35f0b195e7a9c02d7b8
umair <muhammadumair2511@gmail.com>
2023-08-03 12:48:23 +0500

    created table component for dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -13,6 +13,7 @@ import {
 } from "recharts";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
+import Table from "./Table";
 
 const Dashboard = () => {
   const [user, setUser] = useState([]);
@@ -202,55 +203,7 @@ const Dashboard = () => {
               </LineChart>
             </ResponsiveContainer>
             <div className="my-8 w-full">
-              {!isLoading && (
-                <table className="w-full text-sm text-gray-500 text-center rounded">
-                  <thead className="text-xs uppercase bg-clr-700 text-clr-100 ">
-                    <tr>
-                      <th className="px-6 py-3">WPM</th>
-                      <th className="px-6 py-3">Accuracy</th>
-                      <th className="px-6 py-3">Words</th>
-                      <th className="px-6 py-3">Mistakes</th>
-                      <th className="px-6 py-3">Total time</th>
-                      <th className="px-6 py-3">Date</th>
-                      <th className="px-6 py-3">Mode</th>
-                    </tr>
-                  </thead>
-                  <tbody className="text-clr-100">
-                    {results.map((result, index) => (
-                      <tr
-                        key={index}
-                        className={
-                          index % 2
-                            ? "bg-clr-690 border-b border-clr-400"
-                            : "bg-clr-650 border-b border-clr-400"
-                        }
-                      >
-                        <td className="px-6 py-4 font-medium whitespace-nowrap">
-                          {Math.round(
-                            result.length /
-                              ((result.endTime - result.startTime) / 60000)
-                          )}
-                        </td>
-                        <td className=" px-6 py-4">
-                          {Math.round(result.acc)}%
-                        </td>
-                        <td className=" px-6 py-4">{result.length}</td>
-                        <td className=" px-6 py-4">{result.mistakes}</td>
-                        <td className="px-6 py-4">
-                          {Math.round(
-                            (result.endTime - result.startTime) / 1000
-                          )}
-                          s
-                        </td>
-                        <td className="px-6 py-4">
-                          {new Date(result.startTime).toLocaleString()}
-                        </td>
-                        <td className="px-6 py-4">{result.mode}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              )}
+              {!isLoading && <Table data={results} />}
             </div>
           </div>
           <div className="grid grid-cols-4 grid-rows-2 px-16 gap-4 my-8">
